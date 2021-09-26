0bcb11e04c3f9be3f9c24ac4775905b3e0945ed1
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2021-09-27 00:31:16 +0900

    laravel sutudy

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -28,3 +28,22 @@ class ProfileController extends Controller
         return redirect('admin/profile/edit');
     }
 }
+/*
+・ControllerとRoutingについてわからないこと
+publicという記載はこのclassメソッドがどこからでも参照できます
+みたいな認識なのですがpublicとつけない場合もあるのでしょうか？
+
+・Controllerの役割
+コントローラーはMVCのアプリでは処理全般を定義する部分であり
+データを受け渡す、保存するといった処理のみを行います。
+画面の部分の処理などはviewが担当するといった形にすることで
+コードがわかりやすくなると認識しています。
+
+・ControllerとRoutingの役割
+前述のとおりコントローラーが処理を行いviewを呼び出すもので
+ルーティングはどのようなhttp通信が来た場合にどのコントローラーに
+処理を渡すかを定義するものであると認識しています。
+ルーティング→コントローラー→view
+の順番で処理が行われると考えています。
+
+*/
