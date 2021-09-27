e905152258b539e6a00bb876fd026d341189ab02
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2021-09-27 16:51:30 +0900

    laravel study

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create', 'Admin\NewsController@add');
+
+    // 課題追加分
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+// 課題追加分
+Route::get('XXX','AAAController@bbb');
+
+
+/*
+URLとControllerやActionをひも付ける機能を何といいますか？
+ルーティングと言います。
+routes/web.phpで定義します。
+
+あなたが考える、group化をすることのメリットを考えてみてください
+admin/から始まるURLをまとめておけるため可読性が上がると考えます。
+*/
